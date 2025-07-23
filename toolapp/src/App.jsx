afa0e37ccfc3964a446468e086ada2a2ca0c0fbf
riyaverma85 <riyaverma2976@gmail.com>
2025-07-23 02:58:40 -0700

    first commit

---
+++ toolapp/src/App.jsx
@@ -0,0 +1,11 @@
+import Todo from "./Todo";
+
+const App=()=>{
+  return(
+    <>
+      <h1 id="top">Todo List!!</h1>
+      <Todo/>
+    </>
+  )
+}
+export default App;
---
+++ toolapp/src/Todo.jsx
@@ -0,0 +1,41 @@
+import Table from 'react-bootstrap/Table';
+import { useSelector,useDispatch } from "react-redux";
+import { addTask } from "./todoSlice";
+import { useState } from "react";
+const Todo=()=>{
+    const data=useSelector((state)=>state.todo.task)
+    const dispatch=useDispatch();
+    const [txt,setTxt]=useState("")
+    let sno=0;
+    const ans=data.map((key)=>{
+        sno++;
+        return(
+            <>
+            <tr>
+                <td>{sno}</td>
+                <td>{key.work}</td>
+            </tr>
+            </>
+        )
+    })
+    return(
+        <>
+        <div className='taskbox'>
+          Enter Task: <input type="text" value={txt} onChange={(e)=>{setTxt(e.target.value)}}/><br/><br/>
+        <button onClick={()=>{dispatch(addTask({work:txt}))}}>Add</button><br/><br/>
+        </div>
+    <Table striped bordered hover size="sm" variant='light'>
+      <thead>
+        <tr>
+          <th>Sno</th>
+          <th>Task</th>
+        </tr>
+      </thead>
+      <tbody>
+        {ans}
+      </tbody>
+    </Table>
+        </>
+    )
+}
+export default Todo;
